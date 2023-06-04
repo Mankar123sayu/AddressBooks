00ae9a677d2a5edf86202455b259ee450bef6bca
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-04 15:22:18 +0530

    Add

--- AddressBooks/Program.cs
+++ AddressBooks/Program.cs
@@ -8,11 +8,17 @@ namespace AddressBookProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            AddressBokkMain main = new AddressBokkMain();
-           // main.AddContact();
-           // main.Display();
-            main.DeleteContact();
 
+            
+            AddressBokkMain main = new AddressBokkMain();
+
+            Console.Write("Enter the number of contact you want to save: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                main.AddContact();
+                main.Display();
+            }
         }
     }
 }
